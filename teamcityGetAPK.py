3f8fc73afec766ee99c2f0a2a313ca5176b3661e
Konstantin Chistyakov <k.chistyakov@city-mobil.ru>
2019-06-11 12:05:06 +0300

    Initial Part3

--- teamcityGetAPK.py
+++ teamcityGetAPK.py
@@ -23,14 +23,6 @@ version = "" # prod or debug
 TEMPZIPNAME = 'temp.zip' # name of temp zip file
 apks = []
 dwnld = False
-helpUsage ='''
-USAGE: teamcityGetAPK.py *arg1* *arg2*
-*arg1* - номер сборки
-*arg2* - версия приложения
- (слово в названии файла .APK, например 'debug' или 'preview')
-*additional args*:
- -d - APK не будет устанавливаться, а только скачается в папку
-'''
 
 def loadSettingsJSON():
 	'''
@@ -84,20 +76,25 @@ def downloadZIP():
 	'''downloads zip with some assemblyNum'''
 	# change last word to preview or prod in href
 	_pathToAPK_ = SETTINGSJSON['teamcityPathToAPKFolder']
-
-	path = '/app/rest/builds'
-	params = dict(locator="branch:default:any,status:SUCCESS,count:500,lookupLimit:1000")
-	jsonResponse = request(path, params)
-	print("Parsing JSON Response")
-	for build in jsonResponse["build"]:
-		if assemblyNum in build["number"]:
-			path = build['href']+'/artifacts/archived'+_pathToAPK_
-			params = dict(locator="pattern:**/*.apk")
-			streamResponse = request(path, params)
-			SaveZipFromResponse(streamResponse)
-			break
-	else:
+	href = "/app/rest/builds/branch:default:any,number:"+assemblyNum
+	try:
+		path = href+'/artifacts/archived'+_pathToAPK_
+		params = dict(locator="pattern:**/*.apk")
+		streamResponse = request(path, params)
+		SaveZipFromResponse(streamResponse)
+	except:
 		raise Exception("No assembly number is found in Teamcity's assembly numbers")
+
+def clear_zips_folder(zf_path):
+	folder = zf_path
+	for the_file in os.listdir(folder):
+		file_path = os.path.join(folder, the_file)
+		try:
+			if os.path.isfile(file_path):
+				os.unlink(file_path)
+		except Exception as e:
+			print(e)
+	print("Folder with zip files is clear.")
 
 def unziip_file(name=TEMPZIPNAME):
 	global apks
@@ -106,6 +103,8 @@ def unziip_file(name=TEMPZIPNAME):
 
 	try: os.mkdir(apksPath)
 	except FileExistsError: pass
+
+	clear_zips_folder(zf_path=apksPath)
 
 	with ZipFile(name, 'r') as zf:
 		for el in zf.namelist():
@@ -143,24 +142,12 @@ def install_apk():
 					success = True
 	print("APK is successfully installed to device.")
 
-
-if __name__ == '__main__':
-	if len(sys.argv) == 1 or sys.argv[1].lower() in ('-h','-help', '-d'):
-		print(helpUsage)
-	else:
-		assemblyNum = sys.argv[1]
-		print("Assembly number is set to "+assemblyNum)
-		if len(sys.argv) >= 3:
-			if sys.argv[2] != '-d':
-				version = sys.argv[2]
-				print("APK version is set to "+version+'\n')
-			if '-d' in sys.argv:
-				dwnld = True
-				print("Только скачиваем APK файлы, не устанавливая")
-			else: dwnld = False
-			if not version: print("Аргумент версии не получен, установлен на дефолт.")
-
-		loadSettingsJSON()
-		downloadZIP()
-		unziip_file()
-		if not dwnld: install_apk()
+def main(assNum, vers, dwnld=False):
+	global assemblyNum, version
+	assemblyNum = assNum
+	version = vers
+	loadSettingsJSON()
+	downloadZIP()
+	unziip_file()
+	if not dwnld:
+		install_apk()
